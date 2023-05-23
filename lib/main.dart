a42a18685cbf43528565c60605e30ff8c6f8e764
sebcrisan <sebastiaancrisan@gmail.com>
2023-05-23 15:15:31 +0200

    feat: add providers, add films widget, add filter widget

--- lib/main.dart
+++ lib/main.dart
@@ -94,6 +94,96 @@ class FilmsNotifier extends StateNotifier<List<Film>> {
   }
 }
 
+// Different status possibilities
+enum FavoriteStatus { all, favorite, notFavorite }
+
+// Favorite status provider that refers to all different status possibilities
+final favoriteStatusProvider = StateProvider<FavoriteStatus>(
+  (ref) => FavoriteStatus.all,
+);
+
+// All films provider
+final allFilmsProvider = StateNotifierProvider<FilmsNotifier, List<Film>>(
+  (ref) => FilmsNotifier(),
+);
+
+// Favorite films provider, utilizes the providers already available to us
+// so it can afford to be a regular provider
+final favoriteFilmsProvider = Provider<Iterable<Film>>(
+  (ref) => ref.watch(allFilmsProvider).where(
+        (film) => film.isFavorite,
+      ),
+);
+
+// Not favorite films
+final notFavoriteFilmsProvider = Provider<Iterable<Film>>(
+  (ref) => ref.watch(allFilmsProvider).where(
+        (film) => !film.isFavorite,
+      ),
+);
+
+// Films list widget
+class FilmsWidget extends ConsumerWidget {
+  // AlwaysAliveProvider is a mixin, shared by Provider and StateNotifierProvider
+  final AlwaysAliveProviderBase<Iterable<Film>> provider;
+
+  const FilmsWidget({required this.provider, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final films = ref.watch(provider);
+    return Expanded(
+        child: ListView.builder(
+      itemCount: films.length,
+      itemBuilder: (context, index) {
+        final film = films.elementAt(index);
+        final favoriteIcon = film.isFavorite
+            ? const Icon(Icons.favorite)
+            : const Icon(Icons.favorite_border);
+        return ListTile(
+          title: Text(film.title),
+          subtitle: Text(film.description),
+          trailing: IconButton(
+            icon: favoriteIcon,
+            onPressed: () {
+              final isFavorite = !film.isFavorite;
+              ref.read(allFilmsProvider.notifier).update(film, isFavorite);
+            },
+          ),
+        );
+      },
+    ));
+  }
+}
+
+// Filter widget
+class FilterWidget extends StatelessWidget {
+  const FilterWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) {
+        // enum split returns favoritestatus.all, movies.all, etc so we split by the dot
+        return DropdownButton(
+          value: ref.watch(favoriteStatusProvider),
+          items: FavoriteStatus.values
+              .map(
+                (fs) => DropdownMenuItem(
+                  value: fs,
+                  child: Text(fs.toString().split('.').last),
+                ),
+              )
+              .toList(),
+          onChanged: (fs) {
+            ref.read(favoriteStatusProvider.notifier).state = fs!;
+          },
+        );
+      },
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
